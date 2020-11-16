36fd6b5761a715c41a0909a2944bc089340f0d74
Anthony <anthony@9480m.local>
2020-11-16 12:23:37 -0300

    Add and implement fonts in project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
- 
+        <div>   
+            <p>Welcome to Next.js!</p>
+
+            <h2 className="subheading">
+                Learn how to add fonts in a React Project
+            </h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <hr />
+            <h1>Heading 1</h1>
+            <h2>Heading 2</h2>
+            <h3>Heading 3</h3>
+
+            
+            <style jsx>
+                {`
+                    @font-face{
+                        font-family: 'Quicksand-Font';
+                        src: url('/fonts/Quicksand-Font.ttf');
+                    }
+                    
+                    p, h1, h2, h3 {
+                        font-family: 'Quicksand-Font';
+                    }
+                `}
+            </style>
         </div>
     )
 }
